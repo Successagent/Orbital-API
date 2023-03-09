7b032c9f6262c17cf90f1d2d5907b984dc639584
Success Agent <miesineagent@gmail.com>
2023-03-09 21:03:40 +0100

    updated

--- models/Product.js
+++ models/Product.js
@@ -5,13 +5,13 @@ const productSchema = new mongoose.Schema(
     name: { type: String, required: true },
     desc: { type: String, required: true },
     category: { type: String, required: true },
-    subcategory: { type: String, required: true },
-    image: { type:Array,required:true},
+    subcategory: { type: String },
+    image: { type: Array, required: true },
     sizes: { type: Array, required: true },
     quantity: { type: Number, default: 1 },
     qty: { type: Number, default: 1 },
     price: { type: Number, required: true },
-    inStock: { type: Boolean, default: false }
+    inStock: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
