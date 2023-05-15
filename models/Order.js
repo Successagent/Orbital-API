c5d47b637e489f7de31d21ae6776e102c62ef914
Success Agent <miesineagent@gmail.com>
2023-05-15 15:45:47 +0100

    updated

--- models/Order.js
+++ models/Order.js
@@ -27,7 +27,7 @@ const orderSchema = new mongoose.Schema(
           type: String,
         },
         sizes: {
-          type: String,
+          type: Array,
         },
         name: {
           type: String,
